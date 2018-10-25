a295ba2f0e72455219239f3af5a94530946e08f1
sheba sow <Shebee2300>
2018-10-25 10:02:33 -0400

    created classes

---
+++ src/Horse.java
@@ -0,0 +1,6 @@
+ public interface Horse {
+    String getName();
+    int getWeight();
+    String toString();
+
+ }
--- src/HorseBarn.java
+++ src/HorseBarn.java
@@ -1,48 +1,35 @@
 public class HorseBarn {
-    public static void main(String[] args)
-    {
-        Horse horse1 = new Mustang("Secretariat", 12000);
-        Horse horse2 = new Mustang("Dusty Trail", 22000);
-        Horse horse3 = new Mustang("Silver", 12000);
+    private Horse[] spaces;
 
-        Horse[] horses = new Horse[6];
-
-        horse[1] = horse1;
-    }
-
-    public static int getWeight(int weight)
-    {
-        this.weight = weight;
-    }
-    public static int getName(String name)
-    {
-        this.name = name;
-    }
-    public int findHorseSpace(String name) {
-        for (int i = 0; i < this.horses.length; i++) {
-            if (this.horses[i]!=null && name.equals(this.horses[i].getName())) {
-                return i;
-            }
-        }
-        return -1;
-    }
     public void consolidate() {
-        for (int i = 0; i < this.horses.length-1; i++) {
-            if (this.horses[i] == null) {
-                for (int j = i+1; j < this.horses.length; j++) {
-                    if (this.horses[j] != null) {
-                        this.horses[i] = this.spaces[j];
-                        this.horses[j] = null;
-                        j = this.horses.length;
+        for (int i = 0; i < this.spaces.length-1; i++) {
+            if (this.spaces[i] == null) {
+                for (int j = i+1; j < this.spaces.length; j++) {
+                    if (this.spaces[j] != null) {
+                        this.spaces[i] = this.spaces[j];
+                        this.spaces[j] = null;
+                        j = this.spaces.length;
                     }
                 }
             }
         }
     }
-}
+    public int findHorseSpace(String name) {
+        for (int i = 0; i < this.spaces.length; i++) {
+            if (this.spaces[i]!=null && name.equals(this.spaces[i].getName())) {
+                return i+1;
+            }
+        }
+        return -1;
+    }
+    public  String toString() {
+        String enter = "";
+        for(int i = 0; i < spaces.length; i++)
+        {
+            enter += "[" + spaces[i] + "] ";
+        }
+        return enter;
+    }
 
-public interface Horse
-{
-    String getName();
-    int getWeight();
+
 }
